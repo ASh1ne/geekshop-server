f8c7678fc062d15e9e68c5afff7e0e9ef735b9a3
Antony Shirshov <anton.shirshov@gmail.com>
2022-02-01 21:41:09 +0500

    Create authapp, authapp.models, urls, templates

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -9,7 +9,7 @@ MODULE_DIR = os.path.dirname(__file__)
 
 def index(request):
     context = {
-        'title': 'geekshop-main',
+        'title': 'Geekshop | Главная',
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -17,7 +17,7 @@ def index(request):
 def products(request):
     #file_path = os.path.join(MODULE_DIR,'fixtures/products.json')
     context = {
-        'title': 'Каталог',
+        'title': 'Geekbrains | Каталог',
     }
     #context['products'] = json.load(open(file_path, encoding='utf-8'))
     context['products'] = Product.objects.all()
